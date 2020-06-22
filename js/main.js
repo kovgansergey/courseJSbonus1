11f8bd4ded4b71103c1f13bfd9c5823ea93288a9
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-06-22 12:05:55 +0700

    сделал Вторую часть

--- js/main.js
+++ js/main.js
@@ -2,20 +2,35 @@
 
 const registerUserBtn = document.getElementById('register-user');
 const usersList = document.getElementById('list');
-let json = localStorage.getItem('usersData');
+const loginBtn = document.getElementById('login');
+const username = document.getElementById('username');
 
+let json = localStorage.getItem('usersData');
 let usersData = JSON.parse(json) || [];
 
+// генерация списка зарегистрированных пользователей
 function render() {
   usersList.textContent = '';
 
   usersData.forEach(function(item) {
     const li = document.createElement('li');
 
-    li.textContent = 'Имя: ' + item.firstName + ', фамилия: ' + item.lastName + ', зарегистрирован: ' + item.regDate;
+    li.innerHTML = '<span>Имя: ' + item.firstName + ', фамилия: ' + item.lastName + ', зарегистрирован: ' + item.regDate + '</span>' +
+                    '<button class="delete-user-button"><img src="img/delete-icon.svg" alt="delete"></button>';
 
     usersList.insertAdjacentElement('beforeend', li);
+
+    // событие на кнопку "удалить пользователя"
+    const deleteUserBtn = li.querySelector('.delete-user-button');
+    deleteUserBtn.addEventListener('click', function() {
+      let pos = usersData.indexOf(item);
+      usersData.splice(pos, 1);
+      render();
+    });
   });
+
+  json = JSON.stringify(usersData);
+  localStorage.setItem('usersData', json);
 }
 
 // регистрация нового пользователя при клике на кнопку
@@ -81,10 +96,39 @@ registerUserBtn.addEventListener('click', function() {
 
   usersData.push(newUser);
 
-  json = JSON.stringify(usersData);
-  localStorage.setItem('usersData', json);
-
   render();
+});
+
+// авторизация пользователя при клике на кнопку
+loginBtn.addEventListener('click', function() {
+  let loginName = prompt('Введите логин');
+  let loginPassword = prompt('Введите пароль');
+  let checkLoginName = false;
+  let checkLoginPassword = false;
+  let pos;
+
+  /* проверка введенного логина с базой пользователей и если логин найден,
+  то только после этого проверяет пароль*/
+  usersData.forEach(function(item) {
+    if (item.login === loginName) {
+      checkLoginName = true;
+      pos = usersData.indexOf(item);
+
+      if (item.password === loginPassword) {
+        checkLoginPassword = true;
+      }
+    }
+  });
+
+  if (checkLoginName && checkLoginPassword) {
+    username.textContent = usersData[pos].firstName;
+  } else if (!checkLoginName) {
+    alert('Пользователь не найден');
+    return;
+  } else {
+    alert('Неверный пароль');
+    return;
+  }
 });
 
 render();
